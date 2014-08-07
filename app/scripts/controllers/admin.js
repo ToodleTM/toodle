b1b0d82bd84f7118fd7fd75fc61a928d52a451ed
Martin Bahier <martin.bahier@gmail.com>
2014-08-07 13:14:03 +0200

    Added a first version of the admin form that allows tweaking the tournament parameters

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('toodleApp')
-    .controller('AdminCtrl', function ($scope, $location, Tournament) {
-        debugger;
-        console.log(Tournament);
+    .controller('AdminCtrl', function ($scope, $location, $http) {
+        var tournamentId = $location.$$path.split('/')[2];
+
+        $http.get('api/tournament/' + tournamentId).success(function (data) {
+            $scope.tournamentInfo = data;
+        });
     }
 );
