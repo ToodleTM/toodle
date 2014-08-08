965c88a0c0cac7b3dde4737f1fdee1d0f2d1d3b3
Martin Bahier <martin.bahier@gmail.com>
2014-08-09 00:12:26 +0200

    Added some E2E tests + E2E source files cleanup

--- test/client/spec/main.js
+++ test/client/spec/main.js
@@ -38,11 +38,45 @@ describe('Homepage', function () {
             //.toEqual('×\nClose\nSomething bad happened, the tournament was not created. Please try again. (Reason : Tournament name must not be blank)');
     });
 
-    it("", function(){
+    it("Sould display an OK message if the tournament has been successfully created", function(){
         browser.get(homeAddress);
         element(by.id('tournamentName')).sendKeys('protractor');
         element(by.id('registerTournamentButton')).click();
-        expect(element(by.id('tourneyCreationOk')).getText()).toMatch(/×\nClose\nThe tournament has been created! You can administer it using this link , and you can send this link to allow your users to enroll./g)
+
+        var tourneyConfirmationBox = element(by.id('tourneyCreationOk'));
+        expect(tourneyConfirmationBox.getText()).toMatch(/×\nClose\nThe tournament has been created! You can administer it using this link , and you can send this link to allow your users to enroll./g)
+    });
+
+    it("Should go to an admin page when a tournament is created and we click on the admin link, then edit settings and validate", function(){
+        browser.get(homeAddress);
+        element(by.id('tournamentName')).sendKeys('protractor');
+        element(by.id('registerTournamentButton')).click();
+
+        var tourneyConfirmationBox = element(by.id('tourneyCreationOk'));
+        expect(tourneyConfirmationBox.getText()).toMatch(/×\nClose\nThe tournament has been created! You can administer it using this link , and you can send this link to allow your users to enroll./g)
+        element(by.id('adminLink')).click();
+        var game = element(by.model('tournamentInfo.game'));
+        var description = element(by.model('tournamentInfo.description'));
+        var engine = element(by.model('tournamentInfo.engine'));
+        var numberOfPlayers = element(by.model('tournamentInfo.numberOfPlayers'));
+        var roundFormat = element(by.model('tournamentInfo.roundFormat'));
+        expect(game.getAttribute('value')).toEqual('');
+        expect(description.getAttribute('value')).toEqual('');
+        expect(engine.getAttribute('value')).toEqual('');
+        expect(numberOfPlayers.getAttribute('value')).toEqual('');
+        expect(roundFormat.getAttribute('value')).toEqual('');
+        game.sendKeys("sc2");
+        description.sendKeys("this is a test");
+        engine.sendKeys("Single elim. bracket w/");
+        numberOfPlayers.sendKeys("16");
+        roundFormat.sendKeys("Best of 7");
+        element(by.id('modifyTournament')).click();
+        //browser.refresh(); // => seems to be broken w/ phantomjs, don't know why right now
+        expect(game.getAttribute('value')).toEqual('sc2');
+        expect(description.getAttribute('value')).toEqual('this is a test');
+        expect(engine.getAttribute('value')).toEqual('2');
+        expect(numberOfPlayers.getAttribute('value')).toEqual('16');
+        expect(roundFormat.getAttribute('value')).toEqual('7');
     });
 
 });
