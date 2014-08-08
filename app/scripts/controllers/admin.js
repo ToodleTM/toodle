71bfa7eb891b73381676321410822101b598ef3d
Martin Bahier <martin.bahier@gmail.com>
2014-08-08 13:35:49 +0200

    Admin can now update tournament data

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('toodleApp')
-    .controller('AdminCtrl', function ($scope, $location, $http) {
+    .controller('AdminCtrl', function ($scope, $location, $http, Tournament) {
         var tournamentId = $location.$$path.split('/')[2];
 
         $http.get('api/tournament/' + tournamentId).success(function (data) {
             $scope.tournamentInfo = data;
         });
+
+        $scope.updateTourney = function(){
+            console.log($scope.tournamentInfo);
+            console.log(Tournament.update);
+            Tournament.update({id:tournamentId}, $scope.tournamentInfo, function(){
+                console.log('Success!');
+            }, function(){
+                console.log("Error /o\\!");
+            })
+        };
     }
 );
