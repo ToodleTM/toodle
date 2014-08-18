b820c115b7dc27c300df9592684faf77857c3179
Hoshin <martin.bahier@gmail.com>
2014-08-18 15:48:52 +0200

    Added / tuned report / unreport functions Revamped the bracket to be a js object instead of an array (eases reading + structure is more appropriate)

--- test/server/gameRules/singleElimSpec.js
+++ test/server/gameRules/singleElimSpec.js
@@ -28,7 +28,7 @@ describe('SingleElim engine', function () {
             //action
             engine.initBracket(playersArray, callbackSpy);
             //assert
-            assert.equal(actual.length, expectedBracketLength);
+            assert.equal(Object.keys(actual).length, expectedBracketLength);
         }
 
         describe('Create the first matches', function () {
@@ -54,17 +54,17 @@ describe('SingleElim engine', function () {
                     actual = data;
                 });
                 //action
-                engine.initBracket([john,jane,bob,alice,franz], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, franz], callbackSpy);
                 //assert
-                assert.equal(actual.length, 4);
-                assert.equal(actual[0].player1.name, 'john');
-                assert.equal(actual[0].player2.name, 'jane');
-                assert.equal(actual[1].player1.name, 'bob');
-                assert.equal(actual[1].player2.name, 'alice');
-                assert.equal(actual[2].player1, null);
-                assert.equal(actual[2].player2, null);
-                assert.equal(actual[3].player2.name, 'franz');
+                assert.equal(Object.keys(actual).length, 4);
+                assert.equal(actual[1].player1.name, 'john');
+                assert.equal(actual[1].player2.name, 'jane');
+                assert.equal(actual[2].player1.name, 'bob');
+                assert.equal(actual[2].player2.name, 'alice');
                 assert.equal(actual[3].player1, null);
+                assert.equal(actual[3].player2, null);
+                assert.equal(actual[4].player2.name, 'franz');
+                assert.equal(actual[4].player1, null);
             });
 
             it('should number each match to ease lookup', function () {
@@ -75,137 +75,183 @@ describe('SingleElim engine', function () {
                     actual = data;
                 });
                 //action
-                engine.initBracket([john,jane,bob,alice,franz], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, franz], callbackSpy);
                 //assert 
-                assert.equal(actual.length, 4);
-                assert.equal(actual[0].number, 1);
-                assert.equal(actual[1].number, 2);
-                assert.equal(actual[2].number, 3);
-                assert.equal(actual[3].number, 4);
+                assert.equal(Object.keys(actual).length, 4);
+                assert.equal(actual[1].number, 1);
+                assert.equal(actual[2].number, 2);
+                assert.equal(actual[3].number, 3);
+                assert.equal(actual[4].number, 4);
             });
         });
         describe('create subsequent matches', function () {
             it('should create the subsequent match for a 2-match bracket', function () {
                 //action
-                engine.initBracket([john,jane,bob,alice], callbackSpy);
+                engine.initBracket([john, jane, bob, alice], callbackSpy);
                 //assert
-                assert.equal(actual.length, 3);
-                assert.equal(actual[2].number, 3);
-                assert.equal(actual[2].player1, null);
-                assert.equal(actual[2].player2, null);
+                assert.equal(Object.keys(actual).length, 3);
+                assert.equal(actual[3].number, 3);
+                assert.equal(actual[3].player1, null);
+                assert.equal(actual[3].player2, null);
             });
 
             it('should be able to handle multiple levels of upcoming matches', function () {
                 //action
-                engine.initBracket([john,jane,bob,alice,peter,franz,cole,patrick], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, peter, franz, cole, patrick], callbackSpy);
                 //assert
-                assert.equal(actual.length, 7);
-                assert.equal(actual[4].number, 5);
-                assert.equal(actual[4].player1, null);
-                assert.equal(actual[4].player2, null);
-                assert.equal(actual[5].number, 6);
+                assert.equal(Object.keys(actual).length, 7);
+                assert.equal(actual[5].number, 5);
                 assert.equal(actual[5].player1, null);
                 assert.equal(actual[5].player2, null);
-                assert.equal(actual[6].number, 7);
+                assert.equal(actual[6].number, 6);
                 assert.equal(actual[6].player1, null);
                 assert.equal(actual[6].player2, null);
+                assert.equal(actual[7].number, 7);
+                assert.equal(actual[7].player1, null);
+                assert.equal(actual[7].player2, null);
             });
         });
-
         describe('link matches', function () {
             it('should indicate next match number for each match', function () {
                 //action
-                engine.initBracket([john,jane,bob,alice,peter,franz,cole,patrick], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, peter, franz, cole, patrick], callbackSpy);
                 //assert
-                assert.equal(actual[0].next, 5);
                 assert.equal(actual[1].next, 5);
-                assert.equal(actual[2].next, 6);
+                assert.equal(actual[2].next, 5);
                 assert.equal(actual[3].next, 6);
+                assert.equal(actual[4].next, 6);
 
-                assert.equal(actual[4].next, 7);
                 assert.equal(actual[5].next, 7);
-                assert.equal(actual[6].next, null);
+                assert.equal(actual[6].next, 7);
+                assert.equal(actual[7].next, null);
             });
 
             it('should handle odd brackets when numbering', function () {
                 //action
-                engine.initBracket([john,jane,bob,alice,peter], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, peter], callbackSpy);
                 //assert
-                assert.equal(actual.length, 4)
-                assert.equal(actual[0].next, 3);
+                assert.equal(Object.keys(actual).length, 4)
                 assert.equal(actual[1].next, 3);
-                assert.equal(actual[2].next, 4);
-                assert.equal(actual[3].next, null);
+                assert.equal(actual[2].next, 3);
+                assert.equal(actual[3].next, 4);
+                assert.equal(actual[4].next, null);
             });
         });
-        describe('Bracket balance when initialing bracket', function(){
-
+        describe('balance bracket when initialing bracket', function () {
         });
     });
     describe('report matches', function () {
-        describe('Unreport match', function(){
-
+        describe('Unreport match', function () {
+            it('should be able to unreport a match and update the bracket accordingly', function () {
+                //setup
+                engine.initBracket([john, jane, bob, alice], callbackSpy);
+                actual[1].score1 = 2;
+                actual[1].score2 = 0;
+                actual[1].complete = true;
+                actual[3].player1 = john;
+                //action
+                engine.unreport(1, actual, callbackSpy);
+                //assert
+                var spyCallParam = callbackSpy.getCall(0).args[1];
+                assert.equal(Object.keys(spyCallParam).length, 3);
+                assert.equal(spyCallParam[3].player1, null);
+                assert.equal(spyCallParam[3].player2, null);
+                assert.equal(spyCallParam[1].complete, false);
+                assert.equal(spyCallParam[1].score1, 0);
+                assert.equal(spyCallParam[1].score2, 0);
+            });
+            it('should only allow unreport if the following match has not been reported', function () {
+                //setup
+                engine.initBracket([john, jane, bob, alice, cole, patrick, peter, franz], callbackSpy);
+                engine.reportWin(1, 2, 0, actual, callbackSpy);
+                engine.reportWin(2, 2, 0, actual, callbackSpy);
+                engine.reportWin(3, 2, 0, actual, callbackSpy);
+                engine.reportWin(4, 2, 0, actual, callbackSpy);
+                engine.reportWin(5, 2, 0, actual, callbackSpy);
+                var unreportCallbackSpy = sinon.spy(function(error, data){});
+                //action
+                engine.unreport(1, actual, unreportCallbackSpy);
+                //assert
+                assert.equal(unreportCallbackSpy.getCall(0).args[0].message, 'nextMatchAlreadyReported');
+            });
+            it('should only allow unreport of completed matches', function(){
+                //setup
+                engine.initBracket([john, jane, bob, alice], callbackSpy);
+                //action
+                engine.unreport(3, actual, callbackSpy);
+                //assert
+                assert.equal(callbackSpy.getCall(1).args[0].message, 'cantUnreportIncompleteMatch');
+            });
         });
-
-        describe('Player order when they move along', function(){
-
-        });
-        describe('reporting', function(){
+        describe('Report match', function () {
             it('should update next match with match winner', function () {
                 //setup
-                engine.initBracket([john,jane,bob,alice,peter], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, peter], callbackSpy);
                 //action
                 engine.reportWin(1, 2, 0, actual, callbackSpy);
 
                 //assert
-                assert.equal(actual[2].player1.name, 'john');
-                assert.equal(actual[0].complete, true);
+                assert.equal(actual[3].player1.name, 'john');
+                assert.equal(actual[1].complete, true);
+                assert.equal(actual[1].score1, 2);
+                assert.equal(actual[1].score2, 0);
             });
+
+//            it('should update the right slot of upcoming match', function(){
+//                //setup
+//                engine.initBracket([john, jane, bob, alice, peter], callbackSpy);
+//                //action
+//                engine.reportWin(2, 2, 0, actual, callbackSpy);
+//
+//                //assert
+//                assert.equal(actual[2].player1, null);
+//                assert.equal(actual[2].player2.name, 'bob');
+//            });
 
             it('should update next match with winners from both related matches', function () {
                 //setup
-                engine.initBracket([john,jane,bob,alice,peter], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, peter], callbackSpy);
                 //action
                 engine.reportWin(1, 2, 0, actual, callbackSpy);
                 engine.reportWin(2, 0, 2, actual, callbackSpy);
 
                 //assert
-                assert.equal(actual[2].player1.name, 'john');
-                assert.equal(actual[2].player2.name, 'alice');
-                assert.equal(actual[0].complete, true);
+                assert.equal(actual[3].player1.name, 'john');
+                assert.equal(actual[3].player2.name, 'alice');
+                assert.equal(actual[1].complete, true);
             });
 
             it('should not allow reporting an already reported match', function () {
                 //setup
-                engine.initBracket([john,jane,bob,alice,peter], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, peter], callbackSpy);
                 engine.reportWin(1, 2, 0, actual, callbackSpy);
                 //action
                 engine.reportWin(1, 0, 2, actual, callbackSpy);
 
                 //assert
                 assert.equal(callbackSpy.getCall(1).args[0], null);
-                assert.equal(callbackSpy.getCall(1).args[1].length, 4);
+                assert.equal(Object.keys(callbackSpy.getCall(1).args[1]).length, 4);
                 assert.equal(callbackSpy.getCall(2).args[0].message, 'alreadyReported');
             });
-            it('should be able to tell if a tournament bracket is over', function(){
+            it('should be able to tell if a tournament bracket is over', function () {
                 //setup
                 engine = new SingleElim;
-                reportWinCallbackSpy = sinon.spy(function (err, data, endOfTournamentFlag) {
+                reportWinCallbackSpy = sinon.spy(function (err, data) {
                     actual = data;
                 });
-                engine.initBracket([john,jane], callbackSpy);
+                engine.initBracket([john, jane], callbackSpy);
                 //action
                 engine.reportWin(1, 0, 2, actual, reportWinCallbackSpy);
 
                 //assert
                 assert.equal(reportWinCallbackSpy.getCall(0).args[0], null);
-                assert.equal(reportWinCallbackSpy.getCall(0).args[1].length, 1);
+                assert.equal(Object.keys(reportWinCallbackSpy.getCall(0).args[1]).length, 1);
                 assert.equal(reportWinCallbackSpy.getCall(0).args[2], true);
             });
 
             it('should be able to tell if a tournament bracket is not over', function () {
                 //setup
-                engine.initBracket([john,jane,bob,alice,peter], callbackSpy);
+                engine.initBracket([john, jane, bob, alice, peter], callbackSpy);
                 //action
                 engine.reportWin(1, 2, 0, actual, callbackSpy);
                 //assert
