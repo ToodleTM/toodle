442445ffa2309d06929aff815d25e2232ee2c0e0
Martin Bahier <martin.bahier@gmail.com>
2014-11-01 00:47:42 +0100

    Signup form now allows to input faction when registering

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -9,13 +9,18 @@ angular.module('toodleApp')
         $http.get('api/play/' + tournamentId).success(function (data) {
             $scope.tournamentInfo = data;
             $scope.playerList = data.players;
+            if($scope.tournamentInfo.game){
+                $http.get('/views/resources/factions.json').success(function(data){
+                    $scope.factions = data[$scope.tournamentInfo.game];
+                });
+            }
         });
 
         $scope.enterTournament = function () {
             $("#registrationKo").hide();
             $("#registrationOk").hide();
             $("#inputNick").val('');
-            $http.put('/api/update-tournament/play', {signupID: $scope.tournamentInfo.signupID, nick: $scope.nick}).success(function(data){
+            $http.put('/api/update-tournament/play', {signupID: $scope.tournamentInfo.signupID, nick: $scope.nick, faction:$scope.faction}).success(function(data){
                 $("#registrationOk").fadeIn();
                 $scope.playerList = data.players;
             }).error(function(error){
