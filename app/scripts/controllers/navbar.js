a707cf6499c2bfc9952e1b2b1f9915effffaafc9
Martin Bahier <martin.bahier@gmail.com>
2014-11-08 09:33:09 +0100

    I18N switch button seems to work fine \o/ Updated some translations in the fr i18n file that definitely weren't in french ...

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -2,16 +2,11 @@
 
 angular.module('toodleApp')
     .controller('NavbarCtrl', function ($scope, $location, $translate, $cookies, $cookieStore) {
-
-        $scope.selectedLanguage = $cookies['toodle-lang']?$cookies['toodle-lang']:'en';
-
+        $scope.selectedLanguage = $cookies['toodle-lang'] && $cookies['toodle-lang'] != 'undefined' ?JSON.parse($cookies['toodle-lang']):'en';
         $scope.switchLanguage = function(newLang){
-
-            console.log(newLang);
             $translate.use(newLang);
             $cookieStore.put('toodle-lang', newLang);
         };
-        console.log($scope.selectedLanguage);
         $scope.switchLanguage($scope.selectedLanguage);
         $("#language").on('change', function(evt){ $scope.switchLanguage(evt.currentTarget.value);$scope.$apply()});
     });
