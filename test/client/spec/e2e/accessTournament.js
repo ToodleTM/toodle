39e00125ebdb36a0606ddf45684369dd28ab9106
Martin Bahier <martin.bahier@gmail.com>
2014-12-28 23:41:55 +0100

    e2e tests to check that the 404 triggered when requesting a tournament  w/ an unknown signupId, are taken into account without crashing the app

--- test/client/spec/e2e/accessTournament.js
+++ test/client/spec/e2e/accessTournament.js
@@ -3,45 +3,42 @@ var homeAddress = 'http://localhost:9042';
 var e2eUtils = require('./e2eUtils.js');
 describe('ADMIN - Unknown tournament', function () {
     it('should get a 404 message if ID is valid but tournament does not exist', function () {
-        browser.get(homeAddress+'/admin/123456789012345678901234');
+        browser.get(homeAddress + '/admin/123456789012345678901234');
         e2eUtils.waitForElementToBeVisible(browser, element, by, 'error');
         expect(element(by.id('error')).getText(), 'No such tournament has been registered, check your URL or go back to the main page to create a new tournament.');
     });
 
     it('should get a 404 message if ID is too long', function () {
-        browser.get(homeAddress+'/admin/123456789012345678901234asdfasdf');
+        browser.get(homeAddress + '/admin/123456789012345678901234asdfasdf');
         e2eUtils.waitForElementToBeVisible(browser, element, by, 'error');
         expect(element(by.id('error')).getText(), 'No such tournament has been registered, check your URL or go back to the main page to create a new tournament.');
     });
 
     it('should get a 404 message if ID is too short', function () {
-        browser.get(homeAddress+'/admin/1234567890123');
+        browser.get(homeAddress + '/admin/1234567890123');
         e2eUtils.waitForElementToBeVisible(browser, element, by, 'error');
         expect(element(by.id('error')).getText(), 'No such tournament has been registered, check your URL or go back to the main page to create a new tournament.');
     });
 
     it('should get a 404 message if ID contains invalid characters', function () {
-        browser.get(homeAddress+'/admin/1234567890123456789012zz');
-        e2eUtils.waitForElementToBeVisible(browser, element, by, 'notFound');
+        browser.get(homeAddress + '/admin/1234567890123456789012zz');
+        e2eUtils.waitForElementToBeVisible(browser, element, by, 'error');
         expect(element(by.id('error')).getText(), 'No such tournament has been registered, check your URL or go back to the main page to create a new tournament.');
     });
 });
 
-describe('USER - Unknown tournament', function(){
-    describe('/play page', function(){
-        it('should get a 404 message if ID is valid but tournament does not exist', function () {
-            browser.get(homeAddress+'/play/16%20players0000000000000');
-            e2eUtils.waitForElementToBeVisible(browser, element, by, 'notFound');
-            expect(element(by.id('notFound')).getText(), 'No such tournament seems to exist, are you sure it\'s the right URL ?');
-        });
+describe('USER - Unknown tournament', function () {
+
+    it('should get a 404 message if ID is valid but tournament does not exist (/play)', function () {
+        browser.get(homeAddress + '/play/16players0000000000000');
+        e2eUtils.waitForElementToBeVisible(browser, element, by, 'notFound');
+        expect(element(by.id('notFound')).getText(), 'No such tournament seems to exist, are you sure it\'s the right URL ?');
     });
 
-    describe('/bracket page', function(){
-        it('should get a 404 message if ID is valid but tournament does not exist', function () {
-            browser.get(homeAddress+'/bracket/16%20players0000000000000');
-            e2eUtils.waitForElementToBeVisible(browser, element, by, 'error');
-            expect(element(by.id('notFound')).getText(), 'No such tournament seems to exist, are you sure it\'s the right URL ?');
-        });
+    it('should get a 404 message if ID is valid but tournament does not exist (/bracket)', function () {
+        browser.get(homeAddress + '/bracket/16players0000000000000');
+        e2eUtils.waitForElementToBeVisible(browser, element, by, 'notFound');
+        expect(element(by.id('notFound')).getText(), 'No such tournament seems to exist, are you sure it\'s the right URL ?');
     });
 
 });
