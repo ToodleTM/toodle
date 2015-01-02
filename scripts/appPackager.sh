4dd66b7c1db33924f268cb533bf0338013c5ed60
Martin Bahier <martin.bahier@gmail.com>
2015-01-02 01:32:00 +0100

    Updated app building script to minify the app when building for use in production (all angular controllers + top level app + libs are concatenated and minified) App building script modifies the index.html file so that it uses the minified app file

--- scripts/appPackager.sh
+++ scripts/appPackager.sh
@@ -4,14 +4,23 @@ TARGET=../dist
 compass compile
 duo app/scripts/utils/pack.js > app/scripts/utils/packed.js
 
+function minifyAppAndGenerateIndex {
+    mkdir .minifyTmp
+    echo "Minifying the app"
+    ./scripts/appMinifier.sh ./app ./.minifyTmp ./scripts
+    echo "Generating index to use the minified app"
+    ./scripts/indexGenerator.sh ./app ./.minifyTmp
+    rm -rf .minifyTmp
+}
+
 function copyAppSource {
     cd app/
     rm -rf "$TARGET"
-    for dirToCreate in config i18n images styles views/resources views/maintenance scripts/utils; do
+    for dirToCreate in config i18n images styles views/resources views/maintenance scripts; do
         mkdir -p $TARGET/app/$dirToCreate
     done
     echo "Copying app source files ..."
-    for pattern in *.{ico,txt} .htaccess i18n/*.json images/*.png scripts/app.js scripts/controllers scripts/utils/packed.js views/index.html views/404.html views/partials views/resources/factions.json views/maintenance/index.html config/piwik.config.js ; do
+    for pattern in *.{ico,txt} .htaccess i18n/*.json images/*.png scripts/app.min.js views/index.html views/404.html views/partials views/resources/factions.json views/maintenance/index.html config/piwik.config.js ; do
         echo "copying $pattern to $TARGET/app/$pattern"
         cp -r $pattern $TARGET/app/$pattern
     done
@@ -36,6 +45,7 @@ function cleanup {
 }
 
 function copySource {
+    minifyAppAndGenerateIndex
     copyAppSource
     copyServerSource
     cleanup
---
+++ scripts/indexGenerator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed "s/app.js/app.min.js/" $1/views/index.html > $2/index.html.1
+sed "s/<script src=\"scripts\/controllers\/main.js\"><\/script>//" $2/index.html.1 > $2/index.html.2
+sed "s/<script src=\"scripts\/utils\/packed.js\"><\/script>//" $2/index.html.2 > $2/index.html.3
+sed "s/<script src=\"scripts\/controllers\/navbar.js\"><\/script>//" $2/index.html.3 > $2/index.html.4
+sed "s/<script src=\"scripts\/controllers\/admin.js\"><\/script>//" $2/index.html.4 > $2/index.html.5
+sed "s/<script src=\"scripts\/controllers\/play.js\"><\/script>//" $2/index.html.5 > $2/index.html.6
+sed "s/<script src=\"scripts\/controllers\/bracket.js\"><\/script>//" $2/index.html.6 > $2/index.html.7
+
+cp $2/index.html.7 $1/views/index.html
+rm $2/index.html.{1,2,3,4,5,6,7}
