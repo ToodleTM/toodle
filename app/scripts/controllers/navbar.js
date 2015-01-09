25c621d04be400b2193fa7c2fc3c85264b6f66cf
Martin Bahier <martin.bahier@gmail.com>
2015-01-09 02:05:10 +0100

    Changed the i18n choice menu to one that displays flags and should catch the eye a little better

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -4,9 +4,16 @@ angular.module('toodleApp')
     .controller('NavbarCtrl', function ($scope, $location, $translate, $cookies, $cookieStore) {
         $scope.selectedLanguage = $cookies['toodle-lang'] && $cookies['toodle-lang'] !== 'undefined' ?JSON.parse($cookies['toodle-lang']):'en';
         $scope.switchLanguage = function(newLang){
+            $scope.selectedLanguage = newLang;
             $translate.use(newLang);
             $cookieStore.put('toodle-lang', newLang);
+            $('#activeLanguage').attr('src', './images/flags/'+$scope.selectedLanguage+'-'+$scope.selectedLanguage.toUpperCase()+'.svg');
+            console.log($('#activeLanguage').attr('src'));
         };
         $scope.switchLanguage($scope.selectedLanguage);
-        $('#language').on('change', function(evt){ $scope.switchLanguage(evt.currentTarget.value);$scope.$apply();});
+
+        $('#languageSelector ul li a').click(function(e){
+            e.preventDefault();
+            $scope.switchLanguage($(this).attr('language'));$scope.$apply();
+        });
     });
