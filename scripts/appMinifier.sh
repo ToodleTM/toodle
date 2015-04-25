48e5922bce4f5f9cb2f97324d6f7aefb5d077069
Hoshin <martin.bahier@gmail.com>
2015-04-25 19:47:09 +0200

    [Deployment] Adjsted scripts to better take "Login w/ twitter into account"

--- scripts/appMinifier.sh
+++ scripts/appMinifier.sh
@@ -2,7 +2,7 @@
 
 echo " ==> Concatenating controllers and removing 'use strict' refs"
 cat $1/scripts/controllers/*.js $1/scripts/utils/packed.js > $2/controllers.js
-sed "s/^'use strict';//g" $2/controllers.js > $2/controllers-clean.js
+sed "s/'use strict';//g" $2/controllers.js > $2/controllers-clean.js
 
 echo " ==> Concat the main app on top of concatenated controllers"
 cat $1/scripts/app.js $2/controllers-clean.js > $2/app.js
