c91b40756cc121f262e63a4db605d37ad194d15f
Hoshin <martin.bahier@gmail.com>
2015-04-25 18:18:49 +0200

    [App & Server] "Login with twitter" integration : added tournament listing menus * Tournaments a user access through an admin URL are added to the "tournaments I admin" page if they're logged in

--- test/server/service/tournamentServiceSpec.js
+++ test/server/service/tournamentServiceSpec.js
@@ -587,4 +587,100 @@ describe('Tournament Service', function () {
         });
     });
 
+    describe('getTournamentDataAndSaveTournamentUserAssociation', function(){
+        it('should call the nextCallback with an error if tournament lookup fails w/ an error', function(){
+            //setup
+            var tournamentService = new TournamentService();
+            var tournamentModel = {findById:function(query, callback){
+               callback(true);
+            }};
+            var nextCallback = sinon.spy();
+            var res = {status:function(){
+                var jsonSpy = function(){};
+                jsonSpy.json = sinon.spy();
+                return jsonSpy;
+            }};
+            sinon.spy(res, 'status');
+            //action
+            tournamentService.getTournamentDataAndSaveTournamentUserAssociation(null, res, tournamentModel, null, null, nextCallback);
+            //assert
+            assert.equal(nextCallback.calledOnce, true);
+            assert.equal(res.status.called, false);
+        });
+
+        it('should return a 404 response if tournament data cannot be found', function(){
+            //setup
+            var tournamentService = new TournamentService();
+            var tournamentModel = {findById:function(query, callback){
+                callback(false, null);
+            }};
+            var nextCallback = sinon.spy();
+            var res = {status:function(){
+                var jsonSpy = function(){};
+                jsonSpy.json = sinon.spy();
+                return jsonSpy;
+            }};
+            sinon.spy(res, 'status');
+            //action
+            tournamentService.getTournamentDataAndSaveTournamentUserAssociation(null, res, tournamentModel, null, null, nextCallback);
+            //assert
+            assert.equal(nextCallback.called, false);
+            assert.equal(res.status.calledOnce, true);
+            assert.equal(res.status.getCall(0).args[0], 404);
+        });
+
+        it('should only return tournamentData if no session is defined', function(){
+            //setup
+            var tournamentService = new TournamentService();
+            var tournamentModel = {findById:function(query, callback){
+                callback(false, {_id:'123'});
+            }};
+            var tournamentUserModel = {findOne:sinon.spy()};
+            var nextCallback = sinon.spy();
+            var req = {session:{}};
+            var jsonSpy = sinon.spy();
+            var res = {status:function(){
+                var json = function(){};
+                json.json = jsonSpy;
+                return json;
+            }};
+            sinon.spy(res, 'status');
+            //action
+            tournamentService.getTournamentDataAndSaveTournamentUserAssociation(req, res, tournamentModel, tournamentUserModel, null, nextCallback);
+            //assert
+            assert.equal(nextCallback.called, false);
+            assert.equal(tournamentUserModel.findOne.called, false);
+            assert.equal(res.status.calledOnce, true);
+            assert.equal(res.status.getCall(0).args[0], 200);
+            assert.equal(jsonSpy.calledOnce, true);
+            assert.deepEqual(jsonSpy.getCall(0).args[0], {_id:'123'});
+        });
+
+        it('should lookup tournamentUser associations if session is defined ', function(){
+            var tournamentService = new TournamentService();
+            var tournamentModel = {findById:function(query, callback){
+                callback(false, {_id:'tournamentId'});
+            }};
+            var tournamentUserModel = {findOne:sinon.spy()};
+            var nextCallback = sinon.spy();
+            var req = {session:{passport:{user:{id:'socialId'}}}};
+            var jsonSpy = sinon.spy();
+            var res = {status:function(){
+                var json = function(){};
+                json.json = jsonSpy;
+                return json;
+            }};
+            sinon.spy(res, 'status');
+            //action
+            tournamentService.getTournamentDataAndSaveTournamentUserAssociation(req, res, tournamentModel, tournamentUserModel, null, nextCallback);
+            //assert
+            assert.equal(nextCallback.called, false);
+            assert.equal(tournamentUserModel.findOne.called, true);
+            assert.deepEqual(tournamentUserModel.findOne.getCall(0).args[0], {socialId:'socialId', tournamentId:'tournamentId'});
+            assert.equal(res.status.calledOnce, true);
+            assert.equal(res.status.getCall(0).args[0], 200);
+            assert.equal(jsonSpy.calledOnce, true);
+            assert.deepEqual(jsonSpy.getCall(0).args[0], {_id:'tournamentId'});
+        });
+    });
 });
