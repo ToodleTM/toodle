0bcea2eeaa668d5842daa2b03e9bdb17f7bf8c46
Hoshin <martin.bahier@gmail.com>
2015-05-07 01:06:33 +0200

    [Server] Added unit tests for the "tournamentUser" data retrieval process

--- test/server/service/tournamentServiceSpec.js
+++ test/server/service/tournamentServiceSpec.js
@@ -656,31 +656,108 @@ describe('Tournament Service', function () {
             assert.deepEqual(jsonSpy.getCall(0).args[0], {_id:'123'});
         });
 
-        it('should lookup tournamentUser associations if session is defined ', function(){
-            var tournamentService = new TournamentService();
-            var tournamentModel = {findById:function(query, callback){
-                callback(false, {_id:'tournamentId'});
-            }};
-            var tournamentUserModel = {findOne:sinon.spy()};
-            var nextCallback = sinon.spy();
-            var req = {session:{passport:{user:{id:'socialId'}}}};
-            var jsonSpy = sinon.spy();
-            var res = {status:function(){
-                var json = function(){};
-                json.json = jsonSpy;
-                return json;
-            }};
-            sinon.spy(res, 'status');
-            //action
-            tournamentService.getTournamentDataAndSaveTournamentUserAssociation(req, res, tournamentModel, tournamentUserModel, null, nextCallback);
-            //assert
-            assert.equal(nextCallback.called, false);
-            assert.equal(tournamentUserModel.findOne.called, true);
-            assert.deepEqual(tournamentUserModel.findOne.getCall(0).args[0], {socialId:'socialId', tournamentId:'tournamentId'});
-            assert.equal(res.status.calledOnce, true);
-            assert.equal(res.status.getCall(0).args[0], 200);
-            assert.equal(jsonSpy.calledOnce, true);
-            assert.deepEqual(jsonSpy.getCall(0).args[0], {_id:'tournamentId'});
+        describe('if session is defined', function(){
+            it('should lookup tournamentUser associations if session is defined ', function(){
+                var tournamentService = new TournamentService();
+                var tournamentModel = {findById:function(query, callback){
+                    callback(false, {_id:'tournamentId'});
+                }};
+                var tournamentUserModel = {findOne:sinon.spy()};
+                var nextCallback = sinon.spy();
+                var req = {session:{passport:{user:{id:'socialId'}}}};
+                var jsonSpy = sinon.spy();
+                var res = {status:function(){
+                    var json = function(){};
+                    json.json = jsonSpy;
+                    return json;
+                }};
+                sinon.spy(res, 'status');
+                //action
+                tournamentService.getTournamentDataAndSaveTournamentUserAssociation(req, res, tournamentModel, tournamentUserModel, null, nextCallback);
+                //assert
+                assert.equal(nextCallback.called, false);
+                assert.equal(tournamentUserModel.findOne.called, true);
+                assert.deepEqual(tournamentUserModel.findOne.getCall(0).args[0], {socialId:'socialId', tournamentId:'tournamentId'});
+                assert.equal(res.status.calledOnce, true);
+                assert.equal(res.status.getCall(0).args[0], 200);
+                assert.equal(jsonSpy.calledOnce, true);
+                assert.deepEqual(jsonSpy.getCall(0).args[0], {_id:'tournamentId'});
+            });
+
+            it('should save new tournamentUser association if session is defined, if association does not exist', function(){
+                var tournamentService = new TournamentService();
+                var tournamentModel = {findById:function(query, callback){
+                    callback(false, {_id:'tournamentId', tournamentName:'tournamentName'});
+                }};
+                var newTournamentUser = {save:function(){
+                    }};
+                sinon.spy(newTournamentUser, 'save');
+                var tournamentUserModel = function(){
+                    return newTournamentUser;
+                };
+                tournamentUserModel.findOne = function(params, callback){
+                    callback(false, null);
+                };
+
+                var nextCallback = sinon.spy();
+                var req = {session:{passport:{user:{id:'socialId'}}}};
+                var jsonSpy = sinon.spy();
+                var res = {status:function(){
+                    var json = function(){};
+                    json.json = jsonSpy;
+                    return json;
+                }};
+                sinon.spy(res, 'status');
+                //action
+                tournamentService.getTournamentDataAndSaveTournamentUserAssociation(req, res, tournamentModel, tournamentUserModel, null, nextCallback);
+                //assert
+                assert.equal(nextCallback.called, false);
+                assert.equal(res.status.calledOnce, true);
+                assert.equal(res.status.getCall(0).args[0], 200);
+                assert.equal(jsonSpy.calledOnce, true);
+                assert.deepEqual(jsonSpy.getCall(0).args[0], {_id:'tournamentId', tournamentName:'tournamentName'});
+                assert.equal(newTournamentUser.save.calledOnce, true);
+                assert.equal(newTournamentUser.socialId, 'socialId');
+                assert.equal(newTournamentUser.name, 'tournamentName');
+                assert.equal(newTournamentUser.tournamentId, 'tournamentId');
+                assert.equal(newTournamentUser.creator, false);
+                assert.equal(newTournamentUser.admin, true);
+            });
+
+            it('should save new tournamentUser association if session is defined, if association does not exist', function(){
+                var tournamentService = new TournamentService();
+                var tournamentModel = {findById:function(query, callback){
+                    callback(false, {_id:'tournamentId', tournamentName:'tournamentName'});
+                }};
+                var newTournamentUser = {save:function(){
+                }};
+                sinon.spy(newTournamentUser, 'save');
+                var tournamentUserModel = function(){
+                    return newTournamentUser;
+                };
+                tournamentUserModel.findOne = function(params, callback){
+                    callback(false, {socialId:'socialId'});
+                };
+
+                var nextCallback = sinon.spy();
+                var req = {session:{passport:{user:{id:'socialId'}}}};
+                var jsonSpy = sinon.spy();
+                var res = {status:function(){
+                    var json = function(){};
+                    json.json = jsonSpy;
+                    return json;
+                }};
+                sinon.spy(res, 'status');
+                //action
+                tournamentService.getTournamentDataAndSaveTournamentUserAssociation(req, res, tournamentModel, tournamentUserModel, null, nextCallback);
+                //assert
+                assert.equal(nextCallback.called, false);
+                assert.equal(res.status.calledOnce, true);
+                assert.equal(res.status.getCall(0).args[0], 200);
+                assert.equal(jsonSpy.calledOnce, true);
+                assert.deepEqual(jsonSpy.getCall(0).args[0], {_id:'tournamentId', tournamentName:'tournamentName'});
+                assert.equal(newTournamentUser.save.called, false);
+            });
         });
     });
 });
