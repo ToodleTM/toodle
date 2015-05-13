c5499c0d05612f52f578ac8242a46fd0e9c38db9
Hoshin <martin.bahier@gmail.com>
2015-05-13 16:53:44 +0200

    [Script] available engines update script now skips engines that don't have a "meta" property

--- scripts/updateEnginesFromFiles.js
+++ scripts/updateEnginesFromFiles.js
@@ -6,12 +6,12 @@ var path = require('path'),
 
 var config = require('../lib/config/config');
 var db = null;
-if(process.env.NODE_ENV !== 'maintenance'){
+if (process.env.NODE_ENV !== 'maintenance') {
     db = mongoose.connect(config.mongo.uri, config.mongo.options);
 }
 
-AvailableEngine.remove({}, function(err){
-    if(err){
+AvailableEngine.remove({}, function (err) {
+    if (err) {
         console.error('Could not read available engines list in DB, aborting.');
         console.error(err);
         process.exit(1);
@@ -21,17 +21,21 @@ AvailableEngine.remove({}, function(err){
         fs.readdirSync(enginesPath).forEach(function (file) {
             if (/(.*)\.(js$)/.test(file)) {
                 var engine = new (require(enginesPath + '/' + file).Engine)();
-                var newEngine = new AvailableEngine();
-                newEngine.version = engine.meta.version;
-                newEngine.description = engine.meta.description;
-                newEngine.compatible = engine.meta.compatible;
-                newEngine.name = engine.meta.name;
-                console.log('Adding ' + file + ' configuration to engine list');
-                enginesToSave.push(newEngine);
+                if (engine.meta) {
+                    var newEngine = new AvailableEngine();
+                    newEngine.version = engine.meta.version;
+                    newEngine.description = engine.meta.description;
+                    newEngine.compatible = engine.meta.compatible;
+                    newEngine.name = engine.meta.name;
+                    console.log('Adding ' + file + ' configuration to engine list');
+                    enginesToSave.push(newEngine);
+                } else {
+                    console.log('Skipping '+file+' (no meta property)');
+                }
             }
         });
-        AvailableEngine.create(enginesToSave, function(err){
-            if(err) {
+        AvailableEngine.create(enginesToSave, function (err) {
+            if (err) {
                 console.error('Error while inserting data in db');
                 console.error(err);
             } else {
