4dba10aa7cc29621d63fb9ffe711c0df4a28ae43
Hoshin <martin.bahier@gmail.com>
2015-07-06 08:08:00 +0200

    [Deployment scripts] Updated index generator so that the number of angular controller files does not matter (no need to add nez controllers anymore)

--- scripts/indexGenerator.sh
+++ scripts/indexGenerator.sh
@@ -3,11 +3,17 @@
 sed "s/app.js/app.min.js/" $1/views/index.html > $2/index.html.1
 sed "s/<script src=\"scripts\/controllers\/main.js\"><\/script>//" $2/index.html.1 > $2/index.html.2
 sed "s/<script src=\"scripts\/utils\/packed.js\"><\/script>//" $2/index.html.2 > $2/index.html.3
-sed "s/<script src=\"scripts\/controllers\/navbar.js\"><\/script>//" $2/index.html.3 > $2/index.html.4
-sed "s/<script src=\"scripts\/controllers\/admin.js\"><\/script>//" $2/index.html.4 > $2/index.html.5
-sed "s/<script src=\"scripts\/controllers\/play.js\"><\/script>//" $2/index.html.5 > $2/index.html.6
-sed "s/<script src=\"scripts\/controllers\/bracket.js\"><\/script>//" $2/index.html.6 > $2/index.html.7
-sed "s/<script src=\"scripts\/controllers\/myTournaments.js\"><\/script>//" $2/index.html.7 > $2/index.html.8
 
-cp $2/index.html.8 $1/views/index.html
-rm $2/index.html.{1,2,3,4,5,6,7,8}
+
+CONTROLLERS=`ls $1/scripts/controllers | grep -v main.js`
+count=3
+for controller in ${CONTROLLERS};do
+    countplus=$(( $count + 1 ))
+    sed "s/<script src=\"scripts\/controllers\/${controller}\"><\/script>//" $2/index.html.${count} > $2/index.html.${countplus}
+    count=$countplus
+done
+
+cp $2/index.html.${countplus} $1/views/index.packed.html
+for tmpFile in $2/index.html.* ; do
+    rm ${tmpFile}
+done
