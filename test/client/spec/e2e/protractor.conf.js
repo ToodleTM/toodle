c850001c6e4b7c0b5e47d5133beb9c5f45266825
Hoshin <martin.bahier@gmail.com>
2015-08-14 18:05:01 +0200

    [General] Removed inaccurate comments in protractor config

--- test/client/spec/e2e/protractor.conf.js
+++ test/client/spec/e2e/protractor.conf.js
@@ -5,16 +5,4 @@ exports.config = {
         'browserName':'firefox'
     }
 };
-// excluded file so that CI does not fail e2e tests
-// i18N.js <== CI protractor / webdriver / whatever version doesn't seem to be compatible w/ cookies
-
-//alt configuration to use w/ a browser with an actual GUI
-// => all the webdriver-manager config
-//exports.config = {
-//    //seleniumAddress: 'http://localhost:4300',
-//    specs: ['bracket.js'],
-//    capabilities:{
-//        'browserName':'firefox'
-//    }
-//};
 
