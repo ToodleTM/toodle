de0f77bb0c8a12577fd92d4083031de18412360a
Hoshin <martin.bahier@gmail.com>
2016-01-02 17:28:25 +0100

    [General] Build scripts refactor * Piwik config (renamed config file, file insertion related shenanigans made through CI) * Renamed $1/$2 to APP_DIR / OUTPUT_DIR in indexGenerator script

--- scripts/indexGenerator.sh
+++ scripts/indexGenerator.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+APP_DIR=$1
+OUTPUT_DIR=$2
 
-sed "s/app.js/app.min.js/" $1/views/index.html > $2/index.html.1
-sed "s/<script src=\"scripts\/controllers\/main.js\"><\/script>//" $2/index.html.1 > $2/index.html.2
-sed "s/<script src=\"scripts\/utils\/packed.js\"><\/script>//" $2/index.html.2 > $2/index.html.3
+sed "s/app.js/app.min.js/" ${APP_DIR}/views/index.html > ${OUTPUT_DIR}/index.html.1
+sed "s/<script src=\"scripts\/controllers\/main.js\"><\/script>//" ${OUTPUT_DIR}/index.html.1 > ${OUTPUT_DIR}/index.html.2
+sed "s/<script src=\"scripts\/utils\/packed.js\"><\/script>//" ${OUTPUT_DIR}/index.html.2 > ${OUTPUT_DIR}/index.html.3
 
 
-CONTROLLERS=`ls $1/scripts/controllers | grep -v main.js`
+CONTROLLERS=`ls ${APP_DIR}/scripts/controllers | grep -v main.js`
 count=3
 for controller in ${CONTROLLERS};do
     countplus=$(( $count + 1 ))
-    sed "s/<script src=\"scripts\/controllers\/${controller}\"><\/script>//" $2/index.html.${count} > $2/index.html.${countplus}
+    sed "s/<script src=\"scripts\/controllers\/${controller}\"><\/script>//" ${OUTPUT_DIR}/index.html.${count} > ${OUTPUT_DIR}/index.html.${countplus}
     count=$countplus
 done
 
-cp $2/index.html.${countplus} $1/views/index.packed.html
-for tmpFile in $2/index.html.* ; do
+cp ${OUTPUT_DIR}/index.html.${countplus} ${APP_DIR}/views/index.packed.html
+for tmpFile in ${OUTPUT_DIR}/index.html.* ; do
     rm ${tmpFile}
 done
